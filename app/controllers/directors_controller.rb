98575284fd712597db0096f411ae1773df1c365a
mfm051 <mateusmatos051@gmail.com>
2023-10-22 10:58:11 -0300

    Adiciona cadastro de diretores

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,5 +2,21 @@ class DirectorsController < ApplicationController
   def show
     @director = Director.find(params[:id])
     @movies_by_director = Movie.where("director_id = ?", params[:id])
-  end 
+  end
+  
+  def new
+    @director = Director.new
+  end
+
+  def create
+    @director = Director.new(params.require(:director).permit(
+      :name,
+      :nationality,
+      :date_of_birth,
+      :genre_id
+    ))
+    @director.save
+
+    redirect_to :root
+  end
 end
