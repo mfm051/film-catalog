498b260e37351c3ae0231229b21defe17f206a13
mfm051 <mateusmatos051@gmail.com>
2023-10-22 09:38:21 -0300

    Adiciona views

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,2 +1,6 @@
 class DirectorsController < ApplicationController
+  def show
+    @director = Director.find(params[:id])
+    @movies_by_director = Movie.where("director_id = ?", params[:id])
+  end 
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,6 @@
 class GenresController < ApplicationController
+  def show
+    @genre = Genre.find(params[:id])
+    @movies_with_genre = Movie.where("genre_id = ?", params[:id])
+  end
 end
