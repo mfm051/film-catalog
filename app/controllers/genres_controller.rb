d5f028c7887bd9685027999aa2c7ef6ae151b627
mfm051 <mateusmatos051@gmail.com>
2023-10-22 10:23:38 -0300

    Adiciona funcionalidade de cadastras novos gêneros

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,15 @@ class GenresController < ApplicationController
     @genre = Genre.find(params[:id])
     @movies_with_genre = Movie.where("genre_id = ?", params[:id])
   end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(params.require(:genre).permit(:name))
+    @genre.save
+
+    redirect_to :root
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @movies = Movie.all
+    @genres = Genre.all
+    @directors = Director.all
   end
 end
