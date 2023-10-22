2318ffe8680e59d54dee33641543011aaea436c6
mfm051 <mateusmatos051@gmail.com>
2023-10-22 11:16:48 -0300

    Adiciona cadastro de filmes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,23 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params.require(:movie).permit(
+      :title,
+      :year,
+      :synopsis,
+      :country,
+      :duration,
+      :director_id,
+      :genre_id
+    ))
+    @movie.save
+
+    redirect_to :root
+  end
 end
