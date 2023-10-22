8c27ecd4ee30f0b689d80256cf73b853960ed4cd
mfm051 <mateusmatos051@gmail.com>
2023-10-22 09:12:04 -0300

    Cria rotas

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: 'home#index'
+  resources :movies
+  resources :directors
+  resources :genres
 end
